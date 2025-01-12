583f10601476bfe111087cc88e9df25adc7a4274
MohamedHegazy2020 <mh219427@gmail.com>
2025-01-12 16:03:46 +0200

    update react-helmet

--- src/components/Pages/Home/index.tsx
+++ src/components/Pages/Home/index.tsx
@@ -2,13 +2,11 @@ import Hero from './Hero';
 import { Helmet } from 'react-helmet';
 
 export default function Home() {
-
   return (
     <section>
       <Helmet>
         <title>Siyam</title>
-
-</Helmet>
+      </Helmet>
       <Hero />
     </section>
   );
