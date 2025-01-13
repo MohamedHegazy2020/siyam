2f528c085d8f841fa9cdf84ecd085b32fb4dae0c
MohamedHegazy2020 <mh219427@gmail.com>
2025-01-13 14:09:12 +0200

    Update styles.ts

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,7 +1,7 @@
 const styles = {
   paddingX: "sm:px-16 px-6",
     paddingY: "sm:py-8 py-4",
-    padding: "sm:px-16 px-6 sm:py-8 py-4",
+    padding: "sm:px-16 px-6 sm:py-16 py-4",
 };
 
 export default styles;
