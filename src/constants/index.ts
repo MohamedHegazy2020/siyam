df721b04b1cc9f2be928a64eb3941f7e613fd1c2
MohamedHegazy2020 <mh219427@gmail.com>
2025-01-20 00:52:47 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -8,14 +8,25 @@ import logo from '../assets/images/logo.png';
 import logoFull from '../assets/images/logo-full.png';
 import factory1 from '../assets/images/factory-1.png';
 import chairman from '../assets/images/chairman.png';
-import qualityPolicy from '../assets/files/Quality-Policy.pdf';
 import group1 from '../assets/images/group-1.png';
 import group2 from '../assets/images/group-2.png';
-export { property1, property2, property3, ibrahimSyam, AbeerSyam, logo, logoFull, factory1 ,usersIcon ,chairman, qualityPolicy ,group1 ,group2};
+import qualityPolicy from '../assets/files/Quality-Policy.pdf';
 
-
-
-
+export {
+  property1,
+  property2,
+  property3,
+  ibrahimSyam,
+  AbeerSyam,
+  logo,
+  logoFull,
+  factory1,
+  usersIcon,
+  chairman,
+  qualityPolicy,
+  group1,
+  group2,
+};
 
 export const tabs = [
   { name: 'Home', path: '/' },
