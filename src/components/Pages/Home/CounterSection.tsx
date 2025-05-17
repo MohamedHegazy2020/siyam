2a94a314c36fe23c6e2d0d6b6407f27266c3d7ef
MohamedHegazy2020 <mh219427@gmail.com>
2025-05-17 13:25:34 +0300

    refactor: clean up formatting and improve toggle actions in CounterSection component

--- src/components/Pages/Home/CounterSection.tsx
+++ src/components/Pages/Home/CounterSection.tsx
@@ -1,29 +1,22 @@
-import React, { useRef } from 'react'
+import React, { useRef } from 'react';
 import styles from '../../../utils/styles';
 import { useGSAP } from '@gsap/react';
 import gsap from 'gsap';
 import StarsCanvas from '../../canvas/Stars';
 
 const CounterSection = () => {
-
-
-
-
   return (
     <div
-      className={
-        ' grid grid-cols-2 md:grid-cols-4 gap-4 bg-cover relative bg-center bg-[#000000e0] ' + styles.padding
-      }
+      className={' grid grid-cols-2 md:grid-cols-4 gap-4 bg-cover relative bg-center bg-[#000000e0] ' + styles.padding}
     >
-      <StarsCanvas/>
+      <StarsCanvas />
       <Counter maxNumber={1500} label={'Products'} />
       <Counter label={'experiance'} maxNumber={15} />
       <Counter label={'cliens'} maxNumber={1200} />
       <Counter label={'users'} maxNumber={1100} />
     </div>
   );
-}
-
+};
 
 interface CounterProps {
   maxNumber: number; // Maximum number to count to
@@ -40,11 +33,9 @@ export const Counter: React.FC<CounterProps> = ({ maxNumber, label }) => {
         trigger: numberRef.current,
         start: 'top 90%',
         end: 'bottom 10%',
-        toggleActions: 'restart pause restart pause',
+        toggleActions: 'play none none none',
       },
     });
-
-
 
     tl.fromTo(
       numberRef.current,
@@ -72,5 +63,4 @@ export const Counter: React.FC<CounterProps> = ({ maxNumber, label }) => {
   );
 };
 
-
-export default CounterSection
+export default CounterSection;
